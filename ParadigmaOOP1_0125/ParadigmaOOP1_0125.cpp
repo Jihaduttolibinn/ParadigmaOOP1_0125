78e7e7bb6189a3f6f3afe0242209e09a363633f0
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-05-20 10:30:37 +0700

    memasukkan class nama

--- ParadigmaOOP1_0125/ParadigmaOOP1_0125.cpp
+++ ParadigmaOOP1_0125/ParadigmaOOP1_0125.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+public:
+	mahasiswa(int pid,string pNama,float pNilai) :id(pid) nama(pNama)
+
+}
+};

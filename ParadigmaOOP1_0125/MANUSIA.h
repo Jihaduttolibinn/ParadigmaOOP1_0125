9a362619b85914fbd7eabfeb8f4c4a277fef56cb
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-05-20 10:58:34 +0700

    MANUSIA

---
+++ ParadigmaOOP1_0125/MANUSIA.h
@@ -0,0 +1,21 @@
+#ifndef MANUSIA_H
+#define MANUSIA_H
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+	string name;
+	jantung varJantung;
+
+	manusia(string pName)
+
+		: nama(Pname)
+		cout << name << " hidup\n";
+}
+~manusia() {
+	cout << name << "mati\n";
+}
+};
+#endif
+	
